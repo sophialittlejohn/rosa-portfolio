6a99732bf43d7f985a5db6c261b4a79147ac1a73
Sophia <littlejohn.sophia@gmail.com>
2021-08-01 23:37:35 +0200

    Bolden active menu item

--- frontend/components/elements/navbar.tsx
+++ frontend/components/elements/navbar.tsx
@@ -1,11 +1,14 @@
 import { ComponentLayoutNavbar } from "../../utils/@types/strapi";
 import CustomLink from "./custom-link";
+import classNames from "classnames";
+import { useRouter } from "next/router";
 
 type NavBarProps = {
   navbar: ComponentLayoutNavbar;
 };
 
 const Navbar = ({ navbar }: NavBarProps) => {
+  const { asPath } = useRouter();
   return (
     <nav>
       <ul className="flex content-center w-full justify-center pt-7 mb-10 md:mb-20">
@@ -15,7 +18,12 @@ const Navbar = ({ navbar }: NavBarProps) => {
               <li key={`${navLink?.id}${Math.random()}`}>
                 {navLink && (
                   <CustomLink link={navLink}>
-                    <div className="text-lg md:text-2xl mx-3 uppercase">
+                    <div
+                      className={classNames(
+                        "text-lg md:text-2xl mx-3 uppercase",
+                        { "font-black": asPath?.includes(navLink?.url || "") }
+                      )}
+                    >
                       {navLink?.text}
                     </div>
                   </CustomLink>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -42,7 +42,7 @@ module.exports = {
         },
         dark: {
           css: {
-            p: {
+            DEFAULT: {
               textColor: "#FFFFFF",
             }
           }
