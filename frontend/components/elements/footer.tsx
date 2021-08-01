7e902c1103579f4c9a00f8fc3fe075eb9ce2587d
Sophia <littlejohn.sophia@gmail.com>
2021-08-01 23:36:03 +0200

    Link to home it footer

--- frontend/components/elements/footer.tsx
+++ frontend/components/elements/footer.tsx
@@ -1,4 +1,6 @@
 import { ComponentLayoutFooter } from "../../utils/@types/strapi";
+import CustomLink from "./custom-link";
+import link from "next/link";
 
 type FooterProps = {
   footer: ComponentLayoutFooter;
@@ -7,9 +9,11 @@ type FooterProps = {
 const Footer = ({ footer }: FooterProps) => {
   return (
     <footer className="py-12 self-end content-center w-full text-center">
-      <div className="prose lg:prose-lg max-w-none font-thin italic text-primaryB text-sm md:text-lg">
-        {footer.smallText}
-      </div>
+      <CustomLink link={{ url: "/", id: "home", text: "home" }}>
+        <div className="prose dark:prose lg:prose-lg max-w-none font-thin italic text-primaryB text-sm md:text-lg">
+          {footer.smallText}
+        </div>
+      </CustomLink>
     </footer>
   );
 };
