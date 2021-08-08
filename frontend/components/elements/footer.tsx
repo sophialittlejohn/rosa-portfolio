af62ade8c142b1acf0b5ddf19ef08866a2951c6a
Sophia <littlejohn.sophia@gmail.com>
2021-08-08 22:23:00 +0200

    Correct footer color on dark pages

--- frontend/components/elements/footer.tsx
+++ frontend/components/elements/footer.tsx
@@ -1,16 +1,25 @@
 import { ComponentLayoutFooter } from "../../utils/@types/strapi";
 import CustomLink from "./custom-link";
-import link from "next/link";
+import classNames from "classnames";
 
 type FooterProps = {
   footer: ComponentLayoutFooter;
+  configuration: Record<string, string>;
 };
 
-const Footer = ({ footer }: FooterProps) => {
+const Footer = ({ footer, configuration }: FooterProps) => {
   return (
     <footer className="py-12 self-end content-center w-full text-center">
       <CustomLink link={{ url: "/", id: "home", text: "home" }}>
-        <div className="prose dark:prose lg:prose-lg max-w-none font-thin italic text-primaryB text-sm md:text-lg">
+        <div
+          className={classNames(
+            "lg:prose-lg max-w-none font-thin italic text-sm md:text-lg",
+            {
+              "dark:prose": configuration.pageColor === "dark",
+              prose: configuration.pageColor === "light",
+            }
+          )}
+        >
           {footer.smallText}
         </div>
       </CustomLink>
