07695918fcb42dddcd6d817c15c496f1f064d3ef
Sophia <littlejohn.sophia@gmail.com>
2021-10-21 22:40:49 +0200

    Improve images and gallery handling

--- frontend/components/sections/gallery.tsx
+++ frontend/components/sections/gallery.tsx
@@ -1,3 +1,4 @@
+import classNames from "classnames";
 import { ComponentSectionsGallery } from "../../utils/@types/strapi";
 import { NextImage } from "../elements/image";
 
@@ -10,15 +11,29 @@ export const Gallery = ({ data }: GalleryProps) => {
         {data?.title && <h2>{data.title}</h2>}
         {data?.subtitle && <h3>{data.subtitle}</h3>}
       </div>
-      {data?.images &&
-        data.images.map((image) => {
-          return (
-            <div className="w-screen">
-              {/* @ts-ignore */}
-              <NextImage media={image} />
-            </div>
-          );
+      <div
+        className={classNames({
+          "grid grid-cols-2 w-full items-center justify-center content-center max-w-3xl mx-auto":
+            !data.fullscreen,
         })}
+      >
+        {data?.images &&
+          data.images.map((image) => {
+            return (
+              <div
+                className={classNames({
+                  "w-screen": data.fullscreen,
+                })}
+              >
+                {/* @ts-ignore */}
+                <NextImage media={image} />
+                {image?.caption && (
+                  <p className="py-4 text-2xl font-light">{image.caption}</p>
+                )}
+              </div>
+            );
+          })}
+      </div>
       {data?.caption && (
         <p className="font-light text-xl mt-8">{data.caption}</p>
       )}
--- frontend/components/sections/hero.tsx
+++ frontend/components/sections/hero.tsx
@@ -14,10 +14,10 @@ const Hero = ({ data }: HeroProps) => {
 
   return (
     <section
-      className={`container flex items-center w-full mt-6 md:mt-0 ${styles}`}
+      className={`container flex items-center w-full mt-6 md:mt-0 ${styles} mx-auto pb-56`}
     >
-      <div className="w-full md:w-xxxl mx-auto prose text-center">
-        {data?.title && <h2>{data.title}</h2>}
+      <div className="w-full md:w-xxxl mx-auto prose lg:prose text-center">
+        {data?.title && <h2 className="uppercase pb-8">{data.title}</h2>}
         {data?.picture && <NextImage media={data.picture} />}
         {data.picture?.caption && (
           <p className="mt-4 md:mt-9 text-lg leading-5 px-4 italic font-light">
